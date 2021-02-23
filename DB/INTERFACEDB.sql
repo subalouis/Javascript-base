fac0fb738bf7c595efab808819c1eba21380464f
Louis Andrei Suba <51257898+subalouis@users.noreply.github.com>
2021-02-23 18:01:12 +0800

    Revert "FINAL for Presentation"
    This reverts commit b434fbe5387f81e23eba1f5971cd5e10d634ab1f.

--- DB/INTERFACEDB.sql
+++ DB/INTERFACEDB.sql
@@ -1,6 +1,3 @@
--- using current database
-USE interface;  
-
 -- creating database and table
 CREATE DATABASE if not exists `interface`;
 
@@ -8,74 +5,55 @@ CREATE TABLE IF NOT EXISTS `users`(
     `lastname` VARCHAR(50),
     `firstname` VARCHAR(50),
     `middleinitial` VARCHAR(5),
-	`studentnumber` INT NOT NULL,
     `email` VARCHAR(50),
+     `studentnumber` int auto_increment,
     `username` VARCHAR(20),
     `password` VARCHAR(100),
     `confirmpassword` VARCHAR(100),
     primary key (`studentnumber`)
-);
+)auto_increment = 2021000;
 
 CREATE TABLE IF NOT exists `admin`(
 	`username` VARCHAR(20) not null,
     `password` VARCHAR(50) not null,
     primary key (`username`)
     );
+   
+
     
-CREATE TABLE `logs` (
-    `username` VARCHAR (20),
-    `studentnumber` INT,
-    `datetime` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (`username`) REFERENCES `admin` (`username`),
-    FOREIGN KEY (`studentnumber`) REFERENCES `users` (`studentnumber`)
+    CREATE TABLE `logs` (
+    `username` VARCHAR (20) NOT NULL , 
+    `datetime` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP 
     );
-    
+
 -- using current database
 USE `interface`;  
 
 -- dropping database and tables
-DROP DATABASE `interface`;
-
 DROP TABLE `users`;
 DROP TABLE `admin`;
 DROP TABLE `logs`;
+DROP DATABASE `interface`;
 
--- deleting table data only
+-- deleting table/database data
 TRUNCATE TABLE `users`;
 TRUNCATE TABLE `admin`;
 TRUNCATE TABLE `logs`;
+TRUNCATE DATABASE `interface`
   
 -- inserting admin details
-INSERT INTO `admin`
+insert into `admin`
 values ('admin','admin');
     
--- manual insertion
-INSERT INTO users (`lastname`,`firstname`,`middleinitial`,`email`,`studentnumber`,`username`,`password`,`confirmpassword`)
-VALUES ('Suba','Louis','L','cs.subalouis@gmail.com',123456789, 'louissuba','passa123','passa123');
-        
-SELECT * FROM users;
-SELECT * FROM admin;
-SELECT * FROM logs;
-select * from users where username = 'louissuba';
-
 -- user insertion syntax
-INSERT INTO `users` (`lastname`,`firstname`,`middleinitial`,`email`,`username`,`password`,`confirmpassword`)
+INSERT INTO users (`lastname`,`firstname`,`middleinitial`,`email`,`,`username`,`password`,`confirmpassword`)
 values ('lastName','firsName','MI','sample@domain.com', 'userName','password1234','passwordConfirm1234');
 
-
--- selecting all from tables  
-   
+-- selecting all from tables     
 SELECT * FROM users;
 SELECT * FROM admin;
-SELECT * FROM log;
 SELECT * FROM logs;
-SELECT * from users where username = 'louissuba';
+select * from users where username = 'louissuba'
 
 -- specific data lookup
 SELECT `username`, `email`, `firsname`, `middleinitial`, `lastname` FROM `users` WHERE `username` = 'username';
-SELECT username FROM users WHERE username = ?; -- registration username check
-SELECT * FROM `users` WHERE `username` = ? AND `password` = ?; -- user login password/username check
-INSERT INTO `logs` (`studentnumber`) VALUES (?); -- inserting user login into logs table
-SELECT * FROM `admin` WHERE `username` = ? AND password = ?; -- admin username and password check
-
-
--- README.md
+++ README.md
@@ -1,36 +1,3 @@
-GROUP 8 - DBMS
- 
- Languages USED: Node.js(server side) MySQL(Database) || HTML and CSS (Interface)
-
- --------------------------------------------
-
- Tools Used 
- IDE: Visual Studio Code
- Database Handling (local): MySQL workbench and PHPmyadmin
- Version Handling/Collaboration: GitHub ()
-
-
----------------------------------------------
-
- -- JS Server Side // Overall head
- Suba, Louis Andrei
-
--- Front
-Capati, Patrick - HTML and CSS
-Intal, Ferwinkle - Design Idea and Workflow
-
-
--- Database Design and Setup
-David, Aljohn
-Buan, John Daniel
-
--- ERD Setup
-Yutuc, Denise Ann 
-Calaguas, Erwin 
-
-
-
-
 # Javascript-base.github.io
 code base for javascript practice
 
